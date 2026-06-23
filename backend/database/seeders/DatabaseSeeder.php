df77ea39c17954009e1659d97163369c236d944d
artnet-takamura <m-takamura@art-net.jp>
2026-06-23 16:29:55 +0900

    マイグレ、シーダーコミット

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,12 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Favorite;
+use App\Models\Order_detail;
+use App\Models\Order;
+use App\Models\Stock;
+
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +22,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+
+        $this->call([
+            FavoriteSeeder::class,
+            OrderDetailSeeder::class,
+            OrderSeeder::class,
+            StockSeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
---
+++ backend/database/seeders/OrderDetailSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; // 追記
+
+
+class OrderDetailSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+                        DB::table('orders')->insert([
+            [
+                'product_id' => 1,
+                'order_id' => 1,
+                'quantity' => 1,
+                'product_price' => 5000,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'product_id' => 2,
+                'order_id' => 2,
+                'quantity' => 1,
+                'product_price' => 2000,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'product_id' => 3,
+                'order_id' => 3,
+                'quantity' => 1,
+                'product_price' => 500,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+
+
+
+    }
+}
