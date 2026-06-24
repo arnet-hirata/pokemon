87d4b5176d2bf804d241f1b3ccf62630153fa756
artnet-takamura <m-takamura@art-net.jp>
2026-06-24 11:12:20 +0900

    コントローラー、ルート変更

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AdminProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// 管理者
+Route::apiResource('admin/products', AdminProductController::class);
