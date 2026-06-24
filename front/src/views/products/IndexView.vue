5fef167dd8b31a2d179c75fc97409a09863a1c53
arnet-hirata <y-hirata@art-net.jp>
2026-06-24 11:51:14 +0900

    App.vue追加、変更

--- front/src/views/products/IndexView.vue
+++ front/src/views/products/IndexView.vue
@@ -2,5 +2,33 @@
     
 </script>
 <template>
+    <div>
+        <div class="about">
+            <h1>新商品</h1>
+        </div>
+        <div>
+            <div>
+                <div v-for="product_image in product_images" :key="product_image.id">
+                    {{ product_image.path }}
+                </div>
+                <div v-for="product in products" :key="product.id">
+                    {{ product.name }}
+                </div>
+                <div v-for="product in products" :key="product.id">
+                    {{ product.text }}
+                </div>
+                <div v-for="product in products" :key="product.id">
+                    {{ product.price }}
+                </div>
+            </div>
+        </div>
+        <div>
+            <button>
 
+            </button>
+        </div>
+    </div>
+    <div>
+
+    </div>
 </template>
